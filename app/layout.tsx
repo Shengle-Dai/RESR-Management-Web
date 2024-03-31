5bfc3591bd5c8513dc82209072330d53f0dbfc4a
Frank Dai <sd924@cornell.edu>
2024-03-31 02:50:29 -0400

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import SideNav from '@/app/ui/sidenav';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+    </html>
+  );
+}
