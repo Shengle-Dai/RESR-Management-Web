6568fca56f79579103d54a7b576d966b9fc4ca82
Frank Dai <sd924@cornell.edu>
2024-04-23 23:47:11 -0400

    presentation setting

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className="bg-gray-800 text-white py-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-lg font-semibold">&copy; Shengle Dai</div>
+        <div className="text-lg font-semibold">&copy; RESR-Group</div>
         <div>
           <Socials />
         </div>
